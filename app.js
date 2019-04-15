b581a55f1b6a033d74ea5fe8c6064ceff483c59d
brunoarruda697 <brunoarruda697@gmail.com>
2019-04-15 09:35:15 -0300

    refatorando os arquivos views e o app.js, também foi instalado a dependencia Nodemon

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/games', function(req, res){
-    res.render('secao/games.ejs');
+app.get('/', function(req, res){
+    res.render('home/index');
 });
 
-app.get('/', function(req, res){
-    res.send('<html><body>Portal de noticias</html></body>');
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia');
+});
+
+app.get('/noticias', function (req, res) {
+    res.render('noticias/noticias');
 });
 
 app.listen(3000, function(){
-    console.log('servidor rodando com Expres');
+    console.log('servidor rodando com Express');
 });
